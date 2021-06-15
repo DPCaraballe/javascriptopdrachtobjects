7895621c653fe92827048f490002283c8d1d9a3c
DPCaraballe <84527568+DPCaraballe@users.noreply.github.com>
2021-06-15 18:18:36 +0200

    Add files via upload

---
+++ opdrachtjavascriptobjects/object.js
@@ -0,0 +1,25 @@
+
+let person  = {
+name: 'Daniel',
+age: 44
+
+};
+
+let evaluations = [7, 10, 9];
+let selectedColors = [];
+selectedColors = ["groen", "blauw", "rood"];
+selectedColors.push("geel");
+selectedColors.push(5);
+
+
+
+console.log(person);
+console.log(person.name); 
+console.log(evaluations);
+console.log(selectedColors);
+console.log(selectedColors.length);
+console.log(selectedColors [0]);
+
+
+
+
